0c8ca7ead5817b6ca643ba498ec0eef1e8d1937d
Absar Alam <absar.alam@teamcomputers.com>
2023-01-31 15:31:49 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-import "package:flutter_catalog/home_page.dart";
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
     );
   }
 }
